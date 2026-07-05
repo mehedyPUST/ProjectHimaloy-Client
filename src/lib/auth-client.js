9c88cda8c713c01e19eb2fafbc66c6a46b301977
Mehedy Hasan <110835185+mehedyPUST@users.noreply.github.com>
2026-07-05 20:05:37 +0600

    Add isManager property to user schema

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -12,6 +12,7 @@ export const authClient = createAuthClient({
                 phone: { type: "string" },
                 dateOfBirth: { type: "string" },
                 image: { type: "string" },
+                isManager: { type: "boolean" },
             },
         }),
     ],
